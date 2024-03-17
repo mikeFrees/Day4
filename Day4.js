737def79c420c735e4c179f4b78cc3f59656c175
Mike Frees <mike_frees@hotmail.com>
2024-03-17 16:25:32 +0100

    init

---
+++ Day4.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fs = require("fs");
+
+//Global parameters
+let input, total = 0;
+
+//main program flow
+getInput();
+input.forEach(element => {
+  cardPoints(element);
+});
+console.log(total);
+
+//retrieving the input of a txt file
+function getInput() {
+  try {
+    const data = fs.readFileSync("input.txt", 'utf-8');
+    return input = data.toString().split('/n');   
+  } catch (err) {
+    throw err;
+  }
+}
+
+//calculates the points of a card
+function cardPoints(card) {
+let points = 0;
+console.log(card);
+return points;
+}
